1d05993c2390f132cc50e99dc963f7b3f5909acf
AzizbekTokhirjonov <azizwebdev77@gmail.com>
2022-02-10 21:43:38 +0100

    adds specific dates

--- src/components/Content/CustomDatePicker.jsx
+++ src/components/Content/CustomDatePicker.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Stack from "@mui/material/Stack";
 import TextField from "@mui/material/TextField";
 import AdapterDateFns from "@mui/lab/AdapterDateFns";
@@ -7,16 +7,26 @@ import DateTimePicker from "@mui/lab/DateTimePicker";
 import { BiCalendarAlt } from "react-icons/bi";
 
 export default function CustomDatePicker({
-  showCalendar,
   setShowCalendar,
+  showCalendar,
   task,
 }) {
-  const [value, setValue] = useState(new Date());
+  const today = new Date();
+  const tomorrow = new Date(today);
+  const thisWeekend = new Date("Sat Feb 05 2022 9:50 AM");
+  const nextWeek = new Date(today);
+  const [value, setValue] = useState(today);
 
   const handleChange = (newValue) => {
     setValue(newValue);
   };
+  function getSaturdayDate() {
+    return new Date(new Date().getTime() - 120 * 60 * 60 * 1000);
+  }
 
+  useState(() => {
+    console.log(value);
+  }, [value]);
   return (
     <LocalizationProvider dateAdapter={AdapterDateFns}>
       <Stack spacing={3} className="mt-4">
@@ -25,11 +35,53 @@ export default function CustomDatePicker({
           value={value}
           onChange={handleChange}
           renderInput={(params) => (
-            <TextField
-              {...params}
-              id="date-picker"
-              style={showCalendar ? { display: "none" } : { display: "block" }}
-            />
+            <div>
+              <ul className="date-btns">
+                <li
+                  className="btn"
+                  onClick={() => {
+                    setValue(today);
+                    setShowCalendar(true);
+                  }}
+                >
+                  Today
+                </li>
+                <li
+                  className="btn"
+                  onClick={() => {
+                    setValue(tomorrow.setDate(tomorrow.getDate() + 1));
+                    setShowCalendar(true);
+                  }}
+                >
+                  Tomorrow
+                </li>
+                <li
+                  className="btn"
+                  onClick={() => {
+                    setValue(tomorrow.setDate(thisWeekend.getDate() + 7));
+                    setShowCalendar(true);
+                  }}
+                >
+                  This Weekend
+                </li>
+                <li
+                  className="btn"
+                  onClick={() => {
+                    setValue(nextWeek.setDate(tomorrow.getDate() + 7));
+                    setShowCalendar(true);
+                  }}
+                >
+                  Next Week
+                </li>
+              </ul>
+              <TextField
+                {...params}
+                id="date-picker"
+                style={
+                  showCalendar ? { display: "none" } : { display: "block" }
+                }
+              />
+            </div>
           )}
         />
       </Stack>
